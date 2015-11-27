e40413d96071ec7fea5052ee175885fe11d1a501
JockFax32 <foxtjack@gmail.com>
2015-11-27 13:14:53 -0800

    Events Page Layout with angular placeholders

--- js/controllers/events-ctrl.js
+++ js/controllers/events-ctrl.js
@@ -1,2 +1,7 @@
 angular.module('EventsCtrl', []).controller('EventsCtrl', function($scope) {
+	$scope.results = false;
+	$scope.searchQuery=function(){
+		$scope.results = !$scope.results;
+	};
+
 });
