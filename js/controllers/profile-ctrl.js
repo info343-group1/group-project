de10c97fc1defed565f722df6f9568e3d6b7074f
alexsirr <sirra@uw.edu>
2015-11-30 14:01:26 -0800

    start work on profile contoller

--- js/controllers/profile-ctrl.js
+++ js/controllers/profile-ctrl.js
@@ -1,3 +1,7 @@
 angular.module('ProfileCtrl', []).controller('ProfileCtrl', function($scope, Login, Util) {
     $scope.userId = Login.userId;
+    $scope.users = Login.users;
+    $scope.init = function() {
+        console.log($scope.users);
+    }
 });
