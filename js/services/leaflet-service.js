2ffac725a5b60f6256924c975ee7fbf5f01e9459
Alex Sirr <sirra@uw.edu>
2015-12-07 11:34:46 -0800

    few map bugs fixed, also increased zoom on user location

--- js/services/leaflet-service.js
+++ js/services/leaflet-service.js
@@ -2,12 +2,11 @@ angular.module('LeafletService', []).service('Leaflet', ['$firebaseObject', '$fi
     var leafletService = {};
     var map;
     leafletService.drawMap = function() {
+    	map = L.map('map').setView([38.617, -92.284], 4);
         LocationService.getUserLocation(function(pos) {
-        	console.log(pos)
         	if (pos != null) {
-        		map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 8);
-        	} else {
-        		map = L.map('map').setView([38.617, -92.284], 4);
+        		map.remove();
+        		map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 10);
         	}
         	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
         	layer.addTo(map);
