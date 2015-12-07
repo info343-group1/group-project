3b11d88383951ed9c329e91fe76658b656ca848c
Alex Sirr <sirra@uw.edu>
2015-12-07 10:52:25 -0800

    clean up profile controller and allow profile image to be pulled from the given provider

--- js/controllers/profile-ctrl.js
+++ js/controllers/profile-ctrl.js
@@ -1,9 +1,14 @@
 angular.module('ProfileCtrl', []).controller('ProfileCtrl', function($scope, Login, Util) {
 	$scope.userId = Login.userId;
-	// loop through users to find the user that is logged in b/c userId != the hash firebase stores
-	Login.users.forEach(function(user) {
-		if (user.userId == $scope.userId) {
-			$scope.currentUser = user;
+	
+	if ($scope.userId) {
+		var provider = Login.authObj.$getAuth()["provider"];
+		$scope.currentUser = Login.authObj.$getAuth()[provider];
+		$scope.image = "https://upload.wikimedia.org/wikipedia/commons/7/77/SpaceNeedleTopClose.jpg";
+
+		if (Login.authObj.$getAuth()[provider].profileImageURL != null) {
+			$scope.image = Login.authObj.$getAuth()[provider].profileImageURL;
 		}
-	});
+	}
+	
 });
