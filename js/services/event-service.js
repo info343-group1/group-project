128d2eea59a723966b7d5efbf4f68bd0b00c93d5
Jordan Hand <jordanhand@comcast.net>
2015-12-07 11:19:04 -0800

    added comments

--- js/services/event-service.js
+++ js/services/event-service.js
@@ -4,10 +4,23 @@ angular.module('EventService', []).service('Event', ['$firebaseObject', '$fireba
 	var eventRef = Util.firebaseRef.child('events');
 	data.events = $firebaseArray(eventRef);
 
+	/**
+	 * Saves given event as an event in the database
+	 * 
+	 * @param {Object} eventData object to be saved as an event
+	 * 				   in the database.
+	 */
 	data.addEvent = function(eventData) {
 		data.events.$add(eventData);
 	}
 
+	/**
+	 * Adds the given comment to the given event and saves
+	 * to the database
+	 * 
+	 * @param {Object} event to be commented
+	 * @param {Object} comment data
+	 */
 	data.addComment = function(eventData, commentData) {
 		eventData.comments.push(commentData);
 		eventData.$save(eventData);
--- js/services/util-service.js
+++ js/services/util-service.js
@@ -1,8 +1,17 @@
 angular.module('UtilService', []).service('Util', [function() {
 	var util = {};
 
+	// Reference to the firebase database
 	util.firebaseRef = new Firebase("https://info343-group1.firebaseio.com/");
 
+	/**
+	 * Takes a string and converts it to a searchable string to use in
+	 * a REST call. Compliant with Google API standards. Replaces all
+	 * spaces with '+' characters
+	 * 
+	 * @param  {string to be converted}
+	 * @return {converted search string}
+	 */
 	util.toGoogleSearchString = function(address) {
 		var searchArr = address.trim().split(/\s+/);
 		var urlString = "";
