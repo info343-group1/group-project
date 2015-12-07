4983721636e1a4e5dc341a3494255518378a83ef
alexsirr <sirra@uw.edu>
2015-12-07 14:50:02 -0800

    readd user location zoom to map

--- js/services/leaflet-service.js
+++ js/services/leaflet-service.js
@@ -1,14 +1,21 @@
 angular.module('LeafletService', []).service('Leaflet', ['$firebaseObject', '$firebaseArray', 'Util', "LocationService", "Event", function($firebaseObject, $firebaseArray, Util, LocationService, Event) {
     var leafletService = {};
-    leafletService.map;
+    
+    var view = {coordinates: [38.617, -92.284], zoom: 4 }
     leafletService.drawMap = function() {
-        if (leafletService.map) {
-            leafletService.map.remove();
-        }
-        leafletService.map = L.map('map').setView([38.617, -92.284], 4);
-        var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-        layer.addTo(leafletService.map);
-        leafletService.customMap(Event.events);
+        
+        LocationService.getUserLocation(function(pos) {
+            if (leafletService.map) {
+                leafletService.map.remove();
+            }
+            if (pos != null) {
+                view = {coordinates: [pos.coords.latitude, pos.coords.longitude], zoom: 8}
+            } 
+            leafletService.map = L.map('map').setView(view.coordinates, view.zoom);
+            var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+            layer.addTo(leafletService.map);
+            leafletService.customMap(Event.events); 
+        })   
     }
 
     leafletService.customMap = function(data) {
