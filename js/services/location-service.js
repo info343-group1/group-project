1ea4bc090c9717c059aba61069968594f09bbc5e
Alex Sirr <sirra@uw.edu>
2015-12-06 22:41:40 -0800

    hook events up to map

--- js/services/location-service.js
+++ js/services/location-service.js
@@ -4,10 +4,12 @@ angular.module('LocationService', []).service('LocationService', ['$http', 'Util
 	var gmapsKey = 'AIzaSyAIh_YwZhxDikDlI5-T8fUYbSQ8n2rJzyU';
 	var searchURL = 'https://maps.googleapis.com/maps/api/geocode/json?key='+ gmapsKey + '&address=';
 
-	service.getLatLong = function(address) {
+	// added callback due to null errors after calling this function
+	service.getLatLong = function(address, callback) {
 		var searchString = Util.toGoogleSearchString(address);
 		$http.get(searchURL + searchString).success(function(res) {
 			console.log(res);
+			callback(res);
 		});
 	}
 
