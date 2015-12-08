3b7f49360489742b4810f1ae812bb5831d237ab0
Alex Sirr <sirra@uw.edu>
2015-12-08 11:21:15 -0800

    fix text service bug. Old code is just commented out

--- js/services/text-service.js
+++ js/services/text-service.js
@@ -1,9 +1,10 @@
-angular.module('TextService', []).service('PageData', ['$firebaseObject', '$firebaseArray', 'Util', function($scope, $firebaseObject, $firebaseArray, Util) {
+angular.module('TextService', []).service('PageData', ['$firebaseObject', '$firebaseArray', 'Util', function($firebaseObject, $firebaseArray, Util) {
     var data= {};
     var textRef = Util.firebaseRef.child('page_data');
 
-    $scope.copy = data.page_data = $firebaseArray(textRef);
-    console.log(data.page_data);
+    // commented out due to it crashing the page
+    // $scope.copy = data.page_data = $firebaseArray(textRef);
+    // console.log(data.page_data);
 
 
 
