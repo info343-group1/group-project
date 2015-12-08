4464cf405abe9f05683fd5b717859d7268312850
Alex Sirr <sirra@uw.edu>
2015-12-08 12:20:22 -0800

    changes to profile page to show name and email, also throw out default firebase image in favor of our image... This can perhaps be changed when creating an account

--- js/controllers/profile-ctrl.js
+++ js/controllers/profile-ctrl.js
@@ -9,7 +9,10 @@ angular.module('ProfileCtrl', []).controller('ProfileCtrl', function($scope, Log
 		$scope.currentUser = Login.authObj.$getAuth()[provider];
 		$scope.image = "https://upload.wikimedia.org/wikipedia/commons/7/77/SpaceNeedleTopClose.jpg";
 
-		if (Login.authObj.$getAuth()[provider].profileImageURL != null) {
+		// default firebase images are too small
+		var firebaseImg = "https://secure.gravatar.com/avatar/"
+
+		if (Login.authObj.$getAuth()[provider].profileImageURL != null && Login.authObj.$getAuth()[provider].profileImageURL.indexOf(firebaseImg) == -1) {
 			$scope.image = Login.authObj.$getAuth()[provider].profileImageURL;
 		}
 	}
