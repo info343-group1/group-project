bddff884c2ac7323e0521d92690fa21770a4932a
Alex Sirr <sirra@uw.edu>
2015-12-08 22:10:21 -0800

    broken comment creation

--- js/controllers/full-event-ctrl.js
+++ js/controllers/full-event-ctrl.js
@@ -1,4 +1,4 @@
-angular.module('FullEventCtrl', []).controller('FullEventCtrl', function($scope, Event, $location) {
+angular.module('FullEventCtrl', []).controller('FullEventCtrl', function($scope, Event, $location, Login, Util, $firebaseArray) {
 	$scope.events = Event.events;
 	// $scope.currEvent = $scope.events[];
 	$scope.events.$loaded(function() {
@@ -8,5 +8,19 @@ angular.module('FullEventCtrl', []).controller('FullEventCtrl', function($scope,
 		var id = splitUrl[splitUrl.length - 1];
 		console.log(splitUrl);
 		$scope.currEvent = $scope.events.$getRecord(id);
+
+		$scope.submitComment = function() {
+			var commentRef = Util.firebaseRef.child('events').child($scope.currEvent.$id).child('comments');
+			console.log($scope.newComment)
+			$scope.comments = $firebaseArray(commentRef);
+			var commentData = {
+				"content": $scope.newComment,
+				"owner": Login.user
+			};
+			console.log(commentData)
+			$scope.comments.$add(commentData).then(function(ref) {
+				var id = ref.key();
+			});
+		}
 	});
 });
